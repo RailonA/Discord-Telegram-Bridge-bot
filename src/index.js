b2d32fbba1cbabd8bb7a01f3d157e3741b263792
RailonA <railonacosta@gmail.com>
2023-02-14 18:54:33 -0500

    Connecting to api.github

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const fs = require("fs");
 const Telegram = require('node-telegram-bot-api');
+const fetch = require('cross-fetch');
+
+
+// Or just: import 'cross-fetch/polyfill';
+
+fetch('//api.github.com/users/lquixada')
+  .then(res => {
+    if (res.status >= 400) {
+      throw new Error("Bad response from server");
+    }
+    return res.json();
+  })
+  .then(user => {
+    console.log(user);
+  })
+  .catch(err => {
+    console.error(err);
+  })
+.then((response) => {
+  console.log(`Received response: ${response.status}`);
+});
+
+console.log("Started request…");
 
 // replace the value below with the Telegram token you receive from @BotFather
 
@@ -12,35 +35,35 @@ const startDate = new Date();
 
 // imports
 const commands = require("./../commands");
+const finnhub_commands = require("./../finnhub_commands");
 
-async function handleMessage(text, args, platformObject) {
+
+async function handleMessage(message, args, platformObject) {
   //sends all od the information plus references to the bots to our command handler
   //this can be further abstracted to multiple command handlers if desired
-  await commands.handle(text, args, platformObject, { telegram: TelegramBot });
+  await finnhub_commands.finnhubClient;
+
+  await commands.handle(message, args, platformObject, { telegram: TelegramBot });
 }
 
-
-
 // Matches "/echo [whatever]"
-TelegramBot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
+TelegramBot.onText(/\/echo (.+)/, (message, match) => {
+  // 'message' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
 
-  const chatId = msg.chat.id;
+  const chatId = message.chat.id;
   const resp = match[1]; // the captured "whatever"
 
   // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
+  TelegramBot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
 
 TelegramBot.on('message', async (message) => {
   if (new Date(message.date * 1000) > startDate && message.text) {
     console.log("Got a telegram message: " + message.text);
-  
+
     const chatId = message.chat.id;
 
     //populate object with Telegram information
@@ -48,8 +71,11 @@ TelegramBot.on('message', async (message) => {
       platform: "telegram",
       userID: message.from.id,
       message: message,
-      chatID: message.chat.idull
+      chatID: message.chat.id
     }
+
+    // send a message to the chat acknowledging receipt of their message
+    TelegramBot.sendMessage(chatId, 'Received your message');
 
     //Used to determine command and parameters
     let args = message.text.toLowerCase().split(" ");
@@ -58,8 +84,6 @@ TelegramBot.on('message', async (message) => {
       args[0] = args[0].split("@")[0]
     }
 
-    // send a message to the chat acknowledging receipt of their message
-    TelegramBot.sendMessage(chatId, 'Received your message');
 
     return await handleMessage(message.text, args, plateformObject);
   } else if (message.text) {
