e2c4d93eda92cec190851065ddef10b6c8cbc9a2
RailonA <railonacosta@gmail.com>
2023-02-28 05:09:59 -0500

    Merged with TelegramHTTPRequest

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,37 @@
 const path = require('path');
-const Dotenv = require('dotenv-webpack')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: './main.js',
-    path: path.resolve(__dirname, './dist'),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
-        test: /\.scss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        
+      },
+      {
+        test: /\.(gif|png|jpg|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              disable: true,
+            },
+          },
+        ],
       },
     ],
   },
-  plugins: [
-    new Dotenv()
-  ]
-  
 };
